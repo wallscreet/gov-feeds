b6500658e9456f32b8908a3dda8542b6bb2e21b0
wallscreet <wallscreet@gmail.com>
2025-08-18 16:30:32 -0500

    data sync

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,22 +1,47 @@
-import path from "path";
-import fs from "fs";
-import DebtToThePenny from "@/components/treasury/DebtToThePenny";
 
-export default function Treasury() {
-    
-    return (
-        <div className="font-sans items-center justify-items-center pt-24">
-            <main className="items-center">
-            
-                <h1 className="text-center text-xl tracking-widest text-[#355e93] uppercase mb-6">
-                US Debt to the Penny
-                </h1>
-                
-                <DebtToThePenny />
-            
-            
-            </main>
-        
-        </div>
-    );
+function page() {
+  return (
+    <div>page</div>
+  )
 }
+
+export default page
+
+
+// import { parseStringPromise } from 'xml2js';
+
+// interface RssItem {
+//   title: string[];
+//   link: string[];
+//   description: string[];
+//   pubDate: string[];
+// }
+
+// async function getRssFeed() {
+//   const response = await fetch('https://www.state.gov/rss-feed/department-press-briefings/feed/');
+//   const xml = await response.text();
+//   const result = await parseStringPromise(xml);
+//   return result.rss.channel[0].item as RssItem[];
+// }
+
+// export default async function EnforcementActions() {
+//   const items = await getRssFeed();
+
+//   return (
+//     <div className="container mx-auto p-4 pt-24">
+//         <h1 className="text-center text-xl tracking-widest text-[#355e93] uppercase mb-2">US Federal Reserve - Press Releases</h1>
+//         <h1 className="text-center text-xl tracking-widest text-[#355e93] uppercase">Enforcement Actions</h1>
+
+//         <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 pt-6">
+//             {items.map((item, index) => (
+//                 <div key={index} className="bg-gray-100 border border-slate-200 shadow-lg rounded-lg p-4">
+//                 <h2 className="text-xl text-[#355e93] font-semibold mb-2">{item.title[0]}</h2>
+//                 <p className="text-gray-600 mb-2">{new Date(item.pubDate[0]).toLocaleDateString()}</p>
+//                 <p className="text-[#355e93] mb-4">{item.description[0].substring(0, 420)}...</p>
+//                 <a href={item.link[0]} target="_blank" rel="noopener noreferrer" className="text-[#355e93] hover:text-blue-800 hover:underline">Read more</a>
+//             </div>
+//         ))}
+//         </div>
+//     </div>
+//   );
+// }
