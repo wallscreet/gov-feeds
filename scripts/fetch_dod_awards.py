12e5aa7c1aa1c67eca38ecbfdca8be7e8c588bf8
wallscreet <wallscreet@gmail.com>
2025-09-29 22:27:21 -0500

    revert grok model

--- scripts/fetch_dod_awards.py
+++ scripts/fetch_dod_awards.py
@@ -308,7 +308,7 @@ def contract_awards_to_master_json(out_path: str, filepath: str):
         # Get structured award
         xclient = XAIClient()
         award_details = xclient.get_structured_response(
-            model="grok-4-fast-non-reasoning",
+            model="grok-3-mini",
             response_format=DodContractInfo,
             content=text,
         )
